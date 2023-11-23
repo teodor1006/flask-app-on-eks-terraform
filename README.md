a00d4cbe50f014e5c9b97afcd9ca18d218ec7a72
teodor1006 <tedi_montana@abv.bg>
2023-11-23 14:39:38 +0200

    terraform files

---
+++ README.md
@@ -0,0 +1,1 @@
+# **Deployment of a Flask Application on Amazon EKS Using Terraform** 
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "cloud-native-cluster"
+}
